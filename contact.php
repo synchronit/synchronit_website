ef15d84a9caf56c744a2b1fd04e9570c316c4256
mperez_synchronit <mperez_synchronit@95855063-63ed-4f28-8453-2ab61d086f52>
2016-03-08 18:03:43 +0000

    fix contact form

--- contact.php
+++ contact.php
@@ -12,13 +12,14 @@ $subject =  $_POST['from'];
 
 
 $mail->isSMTP();
+
 $mail->SMTPDebug = 1;// Set mailer to use SMTP
-$mail->Host = 'smtpout.secureserver.net';  // Specify main and backup server
+$mail->Host = 'mail.synchronit.com';  // Specify main and backup server
 $mail->SMTPAuth = true;    // Enable SMTP authentication
-$mail->Username = 'btobler@synchronit.com';                            // SMTP username
-$mail->Password = 'changeme';                           // SMTP password
-$mail->Port = 465;
-$mail->SMTPSecure = 'ssl';                            // Enable encryption, 'ssl' also accepted
+$mail->Username = 'contact@synchronit.com';                            // SMTP username
+$mail->Password = 'Muchachit0123';                           // SMTP password
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
 
 $mail->From = 'contact@synchronit.com';
 $mail->FromName = 'Synchronit Team';
@@ -49,16 +50,16 @@ $subject =  $_POST['subject'];
 
 $mail->isSMTP();
 $mail->SMTPDebug = 1;// Set mailer to use SMTP
-$mail->Host = 'smtpout.secureserver.net';  // Specify main and backup server
+$mail->Host = 'mail.synchronit.com';  // Specify main and backup server
 $mail->SMTPAuth = true;    // Enable SMTP authentication
-$mail->Username = 'btobler@synchronit.com';                            // SMTP username
-$mail->Password = 'changeme';                           // SMTP password
-$mail->Port = 465;
-$mail->SMTPSecure = 'ssl';                            // Enable encryption, 'ssl' also accepted
+$mail->Username = 'contact@synchronit.com';                            // SMTP username
+$mail->Password = 'Muchachit0123';                           // SMTP password
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
 
 $mail->From = $email;
 $mail->FromName = 'Synchronit';
-$mail->addAddress('mperez@synchronit.com');  // Add a recipient
+$mail->addAddress('contact@synchronit.com');  // Add a recipient
 
 $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
 $mail->isHTML(true);                                  // Set email format to HTML
