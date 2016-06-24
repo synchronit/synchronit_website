7111ea5a1bff230ec3dd824e8563674f9a9b7fad
jvega <jvega.perez86@gmail.com>
2016-06-24 12:45:03 -0400

    Sacando las personas desde el appBase

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
 $.extend({
-    indexView: new function () {
+    indexView: new function() {
         var self = this;
         var smController = new ScrollMagic.Controller();
-        
-        self.initialize = function () {            
+
+        self.initialize = function() {
             attachBehavior();
         };
-        
 
-        var attachBehavior = function () {
-            $('#contact-btn').bind('click', function(){
-                if(checkFields() == false)
-                {
+
+        var attachBehavior = function() {
+            $('#contact-btn').bind('click', function() {
+                if (checkFields() == false) {
                     self.showMessage('You must fill the required fields');
                     return;
                 }
@@ -21,78 +20,87 @@ $.extend({
                     cache: false,
                     data: $('#contact-form').serialize(),
                     dataType: 'json',
-                    success: function (result) {
-                            cleanFields();
-                            $('#answer-token').val(result.token);
-                            $('#answer').attr('placeholder',result.question);
-                        
-                            if(result.success == true){
-                                self.showMessage('You message has been send');
-                            }
-                            else{
-                                self.showMessage('Some problem sending your message');
-                            }
+                    success: function(result) {
+                        cleanFields();
+                        $('#answer-token').val(result.token);
+                        $('#answer').attr('placeholder', result.question);
+
+                        if (result.success == true) {
+                            self.showMessage('You message has been send');
+                        } else {
+                            self.showMessage('Some problem sending your message');
                         }
-                    });
+                    }
+                });
             });
-            
+
             self.getCaptcha();
-            
-            //self.getTeamMembers();          
-            
+
+            self.getTeamMembers();
+
         }
-        
-        self.getCaptcha = function(){
+
+        self.getCaptcha = function() {
             $.ajax({
                 type: 'GET',
                 url: 'contact.php?get_capcha=true',
                 cache: false,
                 //data: $('#contact-form').serialize(),
                 dataType: 'json',
-                success: function (result) {
+                success: function(result) {
                     $('#answer-token').val(result.token);
-                    $('#answer').attr('placeholder',result.question);
+                    $('#answer').attr('placeholder', result.question);
                 }
-                });
+            });
         };
-        
-        self.getTeamMembers = function(){
+
+        self.getTeamMembers = function() {
             $.ajax({
                 type: 'GET',
-                url: 'http://dev.synchronit.com/appbase-webconsole/json?command=Get%20PEOPLE',
+                url: 'http://dev.synchronit.com/appbase-webconsole/json',
                 cache: false,
-                //data: {catIs: catId},
+                data: {
+                    command: ' GET PEOPLE with  isActive = true'
+                },
                 dataType: 'json',
-                success: function (result) {
+                success: function(result) {
                     var headers = result.resultSet.headers;
                     var rows = result.resultSet.rows;
-                    
-                    var panelBlock = '<div class="col-lg-3 col-md-4  col-sm-4 col-xs-6 team-item">';
-                    var closedDiv = '</div>'                    
-                    for(var i=0; i< rows.length; i++){
-                        var background = 'background-image: url(' + /*window.location +*/ './img/team/' + rows[i][4] + ')'
-                        var block = '<div class="ch-item" style=" '+ background +'" ><div class="ch-info-wrap"><div class="ch-info">';
-                        
-                        var infoFront = '<div class="ch-info-front">' + closedDiv;
-                        
+
+                    var panelBlock = '<div class="col-lg-3 col-md-4  col-sm-4 col-xs-6 team-item" style="opacity: 1;">';
+                    var closedDiv = '</div>'
+                    for (var i = 0; i < rows.length; i++) {
+
+                        var block = '<div class="ch-item ch-img-' + rows[i][5] + '" ><div class="ch-info-wrap">';
+
+                        var aLink = '<a href="#" data-toggle="modal" data-target="#' + rows[i][5] + '_modal">';
+                        var info = '<div class="ch-info">';
+                        var infoFront = '<div class="ch-info-front"></div>'
                         var infoBack = '<div class="ch-info-back">'
                         var backUpLine = '<h3>' + rows[i][0] + ' ' + rows[i][1] + '</h3>';
                         var backDownLine = '<p>' + rows[i][3] + '</p>';
                         infoBack += backUpLine + backDownLine + closedDiv
-                        
-                        block += infoFront + infoBack + closedDiv;
+                        info += infoFront + infoBack + closedDiv;
+
+                        block += aLink + info + '</a>' + closedDiv + closedDiv;
                         var element = panelBlock + block + closedDiv;
                         $('.row', '#team-section ').append(element);
                     }
-                    
-                    var tween = TweenMax.staggerFrom(".team-item", 0.8, { opacity: 0, delay: 0.3, ease: Circ.easeOut }, 0.03);
-                    var scene = new ScrollMagic.Scene({ triggerElement: "#team-section" }).setTween(tween).addTo(smController);
+
+                    var tween = TweenMax.staggerFrom(".team-item", 0.8, {
+                        opacity: 0,
+                        delay: 0.3,
+                        ease: Circ.easeOut
+                    }, 0.03);
+                    var scene = new ScrollMagic.Scene({
+                        triggerElement: "#team-section"
+                    }).setTween(tween).addTo(smController);
                 }
             });
         }
     }
 });
 
-$(function () {
+$(function() {
     $.indexView.initialize();
 });
