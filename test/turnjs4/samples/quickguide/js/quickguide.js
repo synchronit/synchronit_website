860fd7b159d08d68c7a07a92fba252b138297f57
fernandoguigou <fernandoguigou@192.168.1.106>
2019-07-30 12:23:00 +0200

    changes

--- test/turnjs4/samples/quickguide/js/quickguide.js
+++ test/turnjs4/samples/quickguide/js/quickguide.js
@@ -1,4 +1,4 @@
-/* Steve jobs' book */
+/* QuickGuide book */
 
 function updateDepth(book, newPage) {
 
@@ -32,7 +32,7 @@ function loadPage(page) {
 
 	$.ajax({url: 'pages/page' + page + '.html'}).
 		done(function(pageHtml) {
-			$('.sj-book .p' + page).html(pageHtml.replace('samples/steve-jobs/', ''));
+			$('.sj-book .p' + page).html(pageHtml.replace('samples/quickguide/', ''));
 		});
 
 }
