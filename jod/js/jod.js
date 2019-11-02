274458302b0ac2ed7205ac3f18f568c2b2ac285e
fernandoguigou <fernandoguigou@fernandos-mbp>
2019-11-02 09:59:13 +0100

    Adding some .* files to zensum

--- jod/js/jod.js
+++ jod/js/jod.js
@@ -1,7 +1,19 @@
 
 var showText = function()
 {
+	$("#make").hide();
 	$("#text").show();
+	$("#buttonMake").removeClass("button-hover");
+	$("#buttonText").addClass("button-hover");
+}
+
+
+var showMake = function()
+{
+	$("#text").hide();
+	$("#make").show();
+	$("#buttonText").removeClass("button-hover");
+	$("#buttonMake").addClass("button-hover");
 }
 
 
